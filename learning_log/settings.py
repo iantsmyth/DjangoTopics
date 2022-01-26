ce3313787f1bdbe7b50e8c21c0f8b6631e55438d
iantsmyth <iansmyth95@gmail.com>
2022-01-25 19:51:05 -0500

    Added custom 404 and 500 error pages.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for learning_log project.
 
@@ -60,7 +62,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
